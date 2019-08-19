17f667d11fb7ca57e61b71f3c14fe0cbf2c6c835
Andi Muhammad Tamirn <andimuhammadtamrin@gamil.com>
2019-08-19 21:03:55 +0800

    Tugas 2 Java Script
    Tugas ini saya kumpulkan untuk NIOMIC

---
+++ tugas2.js
@@ -0,0 +1,12 @@
+var angka = 1;
+var string = "Java Script";
+var boolean = true;
+var null1 = null;
+var ada;
+var array = [1,"Java Script", true, null];
+
+console.log(angka);
+console.log(string);
+console.log(boolean);
+console.log(null1,ada);
+console.log(array);
